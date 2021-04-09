6921a4a58a19016b6c5e17fa1d9d23654fc4f9aa
Ra <rrequero@gmail.com>
2021-04-09 11:09:44 +0200

    1-docker: first part

---
+++ 1-docker/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening in port ${process.env.PORT}`);
+});
